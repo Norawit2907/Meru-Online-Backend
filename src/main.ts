e515112d57570ec971cd61d418b054fbff061f5a
Norawit2907 <norawit2907@gmail.com>
2024-08-28 15:26:07 +0700

    chore fix: swagger httpq

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/swagger/api', app, document);
+  SwaggerModule.setup('backend/swagger/api', app, document);
 
   await app.listen(3000);
 }
